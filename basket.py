826dea3c385f8340ce5555a942551f48fae52c0f
WillPowa <39653827+WillPowa@users.noreply.github.com>
2019-05-29 16:24:59 +0100

    Python basket file
    shopping cart basket product calculation

---
+++ basket.py
@@ -0,0 +1,109 @@
+#import 
+
+
+#my_dict = {"Soup": 65 : "Tin", Bread : 80 :"Loaf", "Milk":130 : "Bottle", "Apple": 100:"unit" }
+
+#my_dict = {"Soup": "0.65" , "Bread" : "0.80", "Milk":"1.30"  , "Apple": "1.00" }
+#for key,val in my_dict.items():
+#    print ("The Price for {} is £{}".format(key,val))
+
+#basket = ["Soup", "Bread", "Milk", "Apple"]
+#soup = 0.65
+#bread = 0.80
+#milk = 1.30
+#apple = 1.00
+#basket = ["soup", "bread", "milk", "apple"]
+#tp = sum(0.65:1.00)
+#print(tp)
+#Total_Price = 0.65 + 0.80 + 1.30 + 1.00
+#print (Total_Price)  
+#basket = []
+#grocery = {"Shop", "Bread", "Milk", "Apple"}
+#grocery = {"Soup": 0.65 , "Bread" : 0.80, "Milk":1.30, "Apple": 1.00}
+#for key,val in grocery.items():
+#    tol_pri = sum(int(val))
+#    print (tol_pri)
+
+#    for grocery in basket:
+ #   tol_pri = sum({}.format(key, val))
+#         print ("Total Price for {} is £{}".format(key,val))
+    
+#    total_price = 
+#for items in basket:
+#    if "Soup" in basket:
+#        print ("Soup is in basket and total price is %Add")
+#    elif : 
+#        print ("Your shopping basket is empty")
+        
+
+
+#[
+#  {
+#    "name": "Soup", "price": 65, "unit": "Tin"
+#  },
+#  {
+#    "name": "Bread", "price": 80, "unit": "Loaf"
+#  },
+#  {
+#    "name": "Milk", "price": 130, "unit": "Bottle"
+#  },
+#  {
+#    "name": "Apples", "price": 100, "unit": "Bag"
+#  }
+#]
+
+#a, b = 0, 1
+#for i in range(1,10):
+#    print (a,b)
+#    a, b = b, a+b
+
+#s, b, m, a = 0.65, 0.80, 1.3, 1.0
+#for i in range(1,10):
+#    print (s,b,m,a)
+#    s, b, m, a = s, s+b, s+b+m, s+b+m+a
+
+#create an empty basket
+basket =["Soup" , "Milk", "Apple" ]
+
+
+#create grocery objects with their pries
+grocery = {"Soup": 0.65 , "Bread" : 0.80, "Milk":1.30, "Apple": 1.00}
+
+
+# S, B, M, A = 0, 1, 2, 3
+S, B, M, A, = 0.65, 0.80, 1.3, 1.0
+#S, B, M, A = S, S+B, S+B+M, S+B+M+A
+
+for key, val in grocery.items():
+        if key in basket:
+                total_price = "{}".format(S+B)
+                print ("Total price for your shopping is £{}".format(S+B))
+                S, B, M, A,  =  S+B+M+A,S+B,S+B+M,S,
+#       else: 
+#                print ("{}".format(val))
+
+#tp = 0.65 + 0.80 + 1.30 + 1.0
+#print(tp)
+
+#def add(grocery):
+#        S, B, M, A = 0.65, 0.80, 1.3, 1.0
+#        for i in range(0, grocery):    
+#        for key,val in grocery.items():
+#                if grocery in basket:
+#                       total=add(basket)
+#                        print (add(
+#                                basket
+#                        ))
+#                        print (" Total Price For Buying {} is £{}".format(key,val))
+#                        S, B, M, A = S, S+B, S+B+M, S+B+M+A
+
+
+#           add(
+#                grocery            
+#            )
+#            print(grocery)
+#            yield "{}:{}".format(s+b+m+a)
+#            yield "price of shopping is {}:{}".format(s+b+m+a)
+#        s, b, m, a = s, s+b, s+b+m, s+b+m+a
+#        for item in add(grocery):
+#            print (item)
